cbc8df7b9f0d53ffd0c4f652accb91a672bdf770
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2019-12-20 21:47:48 -0800

    updates gitignore to exclude db3 files, adds base migration, and installs knex-cleaner dependency

---
+++ data/migrations/20191220211944_addsUsersRolesTicketsCategoriesTables.js
@@ -0,0 +1,74 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('roles', tbl => {
+    tbl.increments();
+
+    tbl.string('name', 128)
+      .unique()
+      .notNullable();
+  })
+  .createTable('users', tbl => {
+    tbl.increments();
+    
+    tbl.integer('role_id')
+      .unsigned()
+      .references('id')
+      .inTable('roles')
+      .notNullable();
+
+    tbl.string('email', 128)
+      .unique()
+      .notNullable();
+    
+    tbl.string('password', 128)
+      .notNullable();
+  })
+  .createTable('categories', tbl => {
+    tbl.increments();
+
+    tbl.string('name', 128)
+      .unique()
+      .notNullable();
+  })
+  .createTable('tickets', tbl => {
+    tbl.increments();
+
+    tbl.integer('student_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .notNullable();
+    
+    tbl.integer('category_id')
+      .unsigned()
+      .references('id')
+      .inTable('categories')
+      .notNullable();
+    
+    tbl.integer('helper_id')
+      .unsigned()
+      .references('id') 
+      .inTable('users')
+
+    tbl.string('title', 128)
+      .notNullable();
+    
+    tbl.string('description', 256)
+      .notNullable();
+    
+    tbl.string('tried', 256)
+      .notNullable();
+    
+    tbl.boolean('resolved')
+      .defaultTo(false)
+      .notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('tickets')   
+    .dropTableIfExists('categories')   
+    .dropTableIfExists('users')   
+    .dropTableIfExists('roles');
+};
--- knexfile.js
+++ knexfile.js
@@ -5,26 +5,33 @@ module.exports = {
   test: {
     client: 'sqlite3',
     connection: {
-      filename: 'devdeskq-test.db3'
+      filename: './data/devdeskq-test.db3'
     },
     migrations: {
       directory: './data/migrations'
     },
-    seeds: {
-      directory: './data/seeds'
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     }
   },
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: 'devdeskq-dev.db3'
+      filename: './data/devdeskq-dev.db3'
     },
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     }
   },
 
