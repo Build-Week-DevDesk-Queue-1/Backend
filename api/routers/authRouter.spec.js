69b46bf59cc9599c7de6d7f10aec47ebf3a67995
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2019-12-21 03:33:28 -0800

    adds tests for /api/auth/login endpoint

--- api/routers/authRouter.spec.js
+++ api/routers/authRouter.spec.js
@@ -163,4 +163,85 @@ describe('Authentication Router', () => {
       expect('token' in response.body).toBe(true);
     })
   })
+
+  describe('POST /login', () => {
+    it('does not allow an empty request body', async () => {
+      const loginObject = {};
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('does not allow login without an email', async () => {
+      const loginObject = { ...loginTemplate };
+      delete loginObject.email;
+      
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('does not allow an empty email', async () => {
+      const loginObject = { ...loginTemplate, email: '' };
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('does not allow an invalid email pattern', async () => {
+      const loginObject = { ...loginTemplate, email: 'test@com' }
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('does not allow login without a password', async () => {
+      const loginObject = { ...loginTemplate };
+      delete loginObject.password;
+      
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('does not allow empty password', async () => {
+      const loginObject = { ...loginTemplate, password: '' };
+      
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('does not allow an invalid password pattern', async () => {
+      const loginObject = { ...loginTemplate, password: 'test' };
+      
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginObject);
+      
+      expect(response.status).toBe(400);
+    })
+
+    it('returns a status code 201 and a user object with a token', async () => {
+      const response = await request(server)
+        .post('/api/auth/login')
+        .send(loginTemplate);
+
+      expect(response.status).toBe(201);
+      expect('user' in response.body).toBe(true);
+      expect('token' in response.body).toBe(true);
+    })
+  })
 })
