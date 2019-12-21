05d3b103213242d949d4a0b697c6f30e201ae0ac
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2019-12-21 11:37:07 -0800

    moves routers and tests to their own folders

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const authRouter = require('./routers/authRouter');
+const authRouter = require('./auth/authRouter');
 
 const server = express();
 
