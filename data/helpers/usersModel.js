f267b0347cbb214dcb8dc55d7751c8fb95565a74
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2019-12-21 01:37:11 -0800

    updates users model to only return non sensitive info on add

--- data/helpers/usersModel.js
+++ data/helpers/usersModel.js
@@ -4,15 +4,23 @@ const find = () => {
   return db('users');
 }
 
-const findBy = property => {
+const findBy = properties => {
+  if (Array.isArray(properties)) {
+    return db('users')
+      .where(...properties)
+  }
+
   return db('users')
-    .where(property);
+    .where(properties);
 }
 
 const add = async user => {
   const [id] = await db('users').insert(user, 'id');
 
-  return findBy({ id }).first();
+  return findBy(['users.id', id])
+    .select('users.id', 'role_id', 'email', 'first_name', 'last_name', 'roles.id as role')
+    .join('roles', 'role_id', 'roles.id')
+    .first();
 }
 
 module.exports = {
