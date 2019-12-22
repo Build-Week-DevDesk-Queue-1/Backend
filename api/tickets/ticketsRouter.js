48e790a9b2b1930df58cf2d13c22db3270196ca3
Nattajohn Rojanasupya <n.rojanasupya@gmail.comm>
2019-12-21 23:51:47 -0800

    refactored GET /tickets endpoint with a nested ternary

--- api/tickets/ticketsRouter.js
+++ api/tickets/ticketsRouter.js
@@ -7,26 +7,23 @@ router.get('/', async (req, res) => {
   const { role_id, id } = req.decoded_token;
   const role = await Roles.findBy({ id: role_id });
 
-  switch (role.name) {
-    case 'Student':
-      return Tickets
-        .findBy({ student_id: id })
-        .then(tickets => {
-          res.status(200).json(tickets);
-        })
-        .catch(error => res.status(500).json({ error }));
+  const queryObject = role === 'Student'
+    ? { student_id: id }
+    : role === 'Helper'
+      ? { helper_id: null, resolved: false }
+      : null
 
-    case 'Helper':
-      return Tickets
-        .findBy({ helper_id: null, resolved: false })
-        .then(tickets => {
-          res.status(200).json(tickets);
-        })
-        .catch(error => res.status(500).json({ error }));
-    
-    default:
-      res.status(500).json({ error: 'unable to fetch tickets at this time' });
+  if (!queryObject) {
+    return res.status(400).json({ message: 'not a valid role' });
   }
+
+  Tickets
+    .findBy(queryObject)
+    .then(tickets => {
+      res.status(200).json(tickets);
+    })
+    .catch(error => res.status(500).json({ error }));
+
 })
 
 router.post('/', checkRole('Student'), (req, res) => {
